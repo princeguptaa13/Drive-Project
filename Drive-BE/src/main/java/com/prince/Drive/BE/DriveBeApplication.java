6f14fe51c2d0cc467dc8017842f3b39f87032999
princeguptaa13 <princegupta0132004@gmail.com>
2026-01-10 14:11:27 +0530

    First Commit

---
+++ Drive-BE/src/main/java/com/prince/Drive/BE/DriveBeApplication.java
@@ -0,0 +1,13 @@
+package com.prince.Drive.BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveBeApplication.class, args);
+	}
+
+}
---
+++ Drive-BE/src/test/java/com/prince/Drive/BE/DriveBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prince.Drive.BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
